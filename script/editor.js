a4ea32ebfbb91b17ffbac71b39d8692f554f746d
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2019-11-23 13:57:05 +0100

    continued js

--- script/editor.js
+++ script/editor.js
@@ -1,39 +1,54 @@
 ﻿window.addEventListener('load', () => {
     const markdownText = document.getElementById('markdownText');
+    const previewText = document.getElementById('previewText');
 
     markdownText.addEventListener('input', () => {
-        previewText.innerHTML = '';
+      previewText.innerHTML = '';
 
-        const lines = markdownText.value.split('\n');
+      const lines = markdownText.value.split('\n');
 
-        for (const line of lines) {
+      for (const line of lines) {
+        if (line.charAt(0) === '#' && line.charAt(1) === ' ') {
+          previewText.appendChild(createHeadline(markdownText));
+        } else if (line.charAt(0) === '-' && line.charAt(1) === ' ' && false) {
+          previewText.appendChild(createUnorderedList(markdownText));
+        } else {
+          const text = document.createElement('p');
+          text.textContent = line;
 
-          console.log(markdownText.value);
-
-          if (line.charAt(0) === '#') {
-            previewText.appendChild(createHeading(markdownText));
-          } else {
-            previewText.innerHTML += markdownText.value;
-          }
+          previewText.appendChild(text);
         }
+      }
     });
 
-    function createHeading(element) {
-        const lines = element.value.split('\n');
+    function createHeadline(element) {
+      const lines = element.value.split('\n');
 
-        for (const line of lines) {
-            const lineSplit = line.split(' ', 2);
+      for (const line of lines) {
+        const lineSplit = line.split(' ', 2);
 
-            if (lineSplit[0].length <= 6) {
-                let newHeadline = document.createElement(`h${lineSplit[0].length}`);
-                console.log(`h${lineSplit[0].length}`);
-                console.log(newHeadline);
-                lineSplit[1] = lineSplit[1].replace('undefined', '')
-                newHeadline.innerHTML = lineSplit[1];
-                return newHeadline;
-            }
-
+        if (lineSplit[0].length <= 6) {
+          let newHeadline = document.createElement(`h${lineSplit[0].length}`);
+          lineSplit[1] = lineSplit[1].replace('undefined', '')
+          newHeadline.textContent = lineSplit[1];
+          return newHeadline;
         }
+      }
     }
 
+    function createUnorderedList(element) {
+      const lines = element.value.split('\n');
+      const ul = document.createElement('ul');
+
+      for (const line of lines) {
+        const lineSplit = line.split(' ', 2);
+        console.log(line);
+        
+        const newListItem = document.createElement('li');
+        newListItem.textContent = lineSplit[1];
+        ul.appendChild(newListItem);
+      }
+      
+      return ul;
+    }
 });
